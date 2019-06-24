044282cf0c98c8cc7710101fec7acf1662a7d57b
shab qadri <iamshab@pm.me>
2019-06-25 00:51:33 +0530

    SuperiorOS bringup

--- superior_bacon.mk
+++ superior_bacon.mk
@@ -21,9 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := lineage_bacon
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := superior_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_BRAND := oneplus
 PRODUCT_MANUFACTURER := OnePlus
